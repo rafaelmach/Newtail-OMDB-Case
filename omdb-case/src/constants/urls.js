5af5f7931dc833840d34329d28ab6b00e606fbf3
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-12-26 11:33:18 -0300

    Estrutura Inicial do Projeto

---
+++ omdb-case/src/constants/urls.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://www.omdbapi.com"
+
+export const POSTER_URL = "http://img.omdbapi.com/"
+
+export const API_KEY = "10e66ce1"
---
+++ omdb-case/src/routes/coordinator.js
@@ -0,0 +1,12 @@
+export const goBack = (history) => {
+    history.goBack()
+}
+
+export const goToHomePage = (history) => {
+    history.push("/")
+}
+
+export const goToDetailsPage = (history, id) => {
+    history.push(`/details/${id}`)
+}
+
